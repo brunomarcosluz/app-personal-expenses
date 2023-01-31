dc483072172e3fee19bad3b7e28e9eee2684407a
Bruno Marcos Luz <brunomarcosluz@gmail.com>
2023-01-31 14:29:02 -0300

    Refatorando código implementando componentização

--- personal_expenses/lib/main.dart
+++ personal_expenses/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:personal_expenses/models/transaction.dart';
+import 'package:personal_expenses/components/transaction_user.dart';
+
 
 void main() => runApp(ExpensesApp());
 
@@ -9,27 +10,12 @@ class ExpensesApp extends StatelessWidget {
   @override 
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage()
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Novo tênis de corrida',
-      value: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de Luz',
-      value: 215,
-      date: DateTime.now(),
-    ),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +24,6 @@ class MyHomePage extends StatelessWidget {
         title: Text('Despesas Pessoais'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -48,38 +33,7 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Column(
-            children: _transactions.map((tr){
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 2,
-                        ),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        tr.value.toString()
-                      ),
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Text(tr.title),
-                        Text(tr.date.toString()),
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList(),
-          )
+          TransactionUser(),
         ],
       )
     );
