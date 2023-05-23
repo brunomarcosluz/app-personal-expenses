0656f227457f035672862b7f5587cf20de78a785
Bruno Marcos Luz <brunomarcosluz@gmail.com>
2023-05-22 21:11:26 -0300

    update transiction_list.dart

--- personal_expenses/lib/components/transaction_list.dart
+++ personal_expenses/lib/components/transaction_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:intl/intl.dart';
+//import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import '../models/transaction.dart';
 
